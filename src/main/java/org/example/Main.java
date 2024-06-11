fbb20fed6e6e5eda7868971d76f1d11e92d7ecde
Onlaperd <Onlaperd@gmail.com>
2024-06-11 14:58:33 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(helloMaven());
+        System.out.println(helloJUnit());
+    }
+
+    public static String helloMaven(){
+        return "Hello Maven";
+    }
+    public static String helloJUnit(){
+        return "Hello JUnit";
+    }
+
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+
+public class MainTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("testing has started");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("test started");
+    }
+
+    @Test
+    public void test_func_helloMaven(){
+        Assertions.assertEquals("Hello Maven", Main.helloMaven());
+    }
+
+    @Test
+    public void test_func_helloJUnit(){
+        Assertions.assertEquals("Hello JUnit", Main.helloJUnit());
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("test ended");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("testing has ended");
+    }
+}
